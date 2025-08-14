b0432c886ee34b1a4cd1b57a7f53a186de0f13fb
dromelvan <dromelvan@users.noreply.github.com>
2025-08-14 15:09:57 +0300

    Added update player photos service method

--- d11/d11_service.py
+++ d11/d11_service.py
@@ -1,5 +1,7 @@
 import os
 import json
+import shutil
+import hashlib
 import logging
 
 from types import SimpleNamespace
@@ -117,3 +119,89 @@ class D11Service:
         
         self.d11_mq_sender.send_update_match_message(match_data, finish)
         logging.info(f"Match data for {match_id} sent to MQ")
+
+
+    def update_player_photos(self, photo_directory, competition_id, season):
+        """
+        Updates player photos by fetching them from the Premier League API.
+        """
+
+        download_directory = os.getenv('PREMIER_LEAGUE_PHOTO_DIRECTORY', 'data/premier-league/photo/')
+        temp_directory = download_directory + "temp"
+        updated_directory = download_directory + "updated"
+        new_directory = download_directory + "new"
+        unknown_directory = download_directory + "unknown"
+
+        photo_file_name_format = "{id}.png"
+
+        if os.path.exists(download_directory):
+            shutil.rmtree(download_directory)
+
+        os.makedirs(temp_directory)
+        os.makedirs(updated_directory)
+        os.makedirs(new_directory)
+        os.makedirs(unknown_directory)        
+
+        for team in self.premier_league_service.get_teams(competition_id, season):
+            logging.info(f"{team.name}")
+
+            players = self.premier_league_service.get_players(competition_id, season, team.stat_source_id)
+            for player in players:
+                image = self.premier_league_service.download_player_photo(player.id)
+
+                if image is None:
+                    # No photo was found on PremierLeague.com
+                    logging.info(f"    {player.name.display}: Photo not found")
+                else:
+                    # Download the file to the temp directory and name it with the Premier League player id
+                    temp_file_name = photo_file_name_format.format(id = player.id)
+
+                    with open(temp_directory + "/" + temp_file_name, "wb") as image_file:
+                        image_file.write(image)
+
+                    # Find a player with the Premier League id in the D11 API
+                    d11_player_json = self.api.get_player_by_premier_league_id(player.id)
+
+                    if d11_player_json is None:
+                        # No player with the Premier League id was found in the D11 API
+                        logging.info(f"    {player.name.display}: Unknown")
+                        os.rename(temp_directory + "/" + temp_file_name,
+                                  unknown_directory + "/" + temp_file_name)
+                    else:
+                        d11_player = json.loads(json.dumps(d11_player_json), object_hook=lambda dictionary: SimpleNamespace(**dictionary))
+                        # A player with the Premier League id was found in the D11 API. Name the final result file
+                        # with the D11 API player id
+                        file_name = photo_file_name_format.format(id = d11_player.id)
+
+                        if os.path.exists(photo_directory + "/" + file_name):
+                            # A photo of the player already exists in the D11 application. Get the md5 of both
+                            # the new and the existing file
+                            temp_md5 = self.get_md5(temp_directory + "/" + temp_file_name)
+                            existing_md5 = self.get_md5(photo_directory + "/" + file_name)
+
+                            if temp_md5 == existing_md5:
+                                # The new photo is the same as the already existing photo. Delete the temp file
+                                os.remove(temp_directory + "/" + temp_file_name)
+                                logging.info(f"    {d11_player.name}: Delete")
+                            else:
+                                # The new photo is not the same as the already existing photo.
+                                # Move the file to the updated directory
+                                os.rename(temp_directory + "/" + temp_file_name,
+                                          updated_directory + "/" + file_name)
+                                logging.info(f"    {d11_player.name}: Update")
+                        else:
+                            # A photo of the player does not already exist in the D11 application.
+                            # Move the file to the new directory
+                            os.rename(temp_directory + "/" + temp_file_name,
+                                      new_directory + "/" + file_name)
+                            logging.info(f"    {d11_player.name}: New")
+
+
+    def get_md5(self, filename):
+        """
+        Computes the MD5 hash of a file.
+        """
+        with open(filename, "rb") as file:
+            data = file.read()
+            md5 = hashlib.md5(data).hexdigest()
+            return md5
